45713e37677fa26fa80c8e21e3beba1e4ce32cb2
Pontus Alexander <pontus.alexander@gmail.com>
2025-09-17 22:44:51 +0200

    Check for null in isIterator and add test
    Fixes TypeError: Cannot use 'in' operator to search for 'Symbol(Symbol.iterator)' in null

--- src/guards.ts
+++ src/guards.ts
@@ -1,3 +1,3 @@
 export function isIterable(value: any): value is Iterable<unknown> {
-  return typeof value === "object" && Symbol.iterator in value;
+  return value != null && typeof value === "object" && Symbol.iterator in value;
 }
